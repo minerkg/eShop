2f19dca2a4e4ed2ba260a9b9fb5d907093ef59bd
Örs Csiszer <csiszerors@gmail.com>
2024-09-08 13:23:45 +0300

    cart item controller implementation

---
+++ src/main/java/com/csiszer/my_shop/controller/CartItemController.java
@@ -0,0 +1,51 @@
+package com.csiszer.my_shop.controller;
+
+import com.csiszer.my_shop.exceptions.ResourceNotFoundException;
+import com.csiszer.my_shop.model.CartItem;
+import com.csiszer.my_shop.response.ApiResponse;
+import com.csiszer.my_shop.service.cart.ICartItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/cartItems")
+public class CartItemController {
+    private final ICartItemService cartItemService;
+
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId,
+                                                     @RequestParam Long productId,
+                                                     @RequestParam Integer quantity) {
+        try {
+            cartItemService.addItemToCart(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Item added successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/cart/{cartId}/item/{itemId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long itemId) {
+        try {
+            cartItemService.removeItemFromCart(cartId,itemId);
+            return ResponseEntity.ok(new ApiResponse("Item removed from the cart", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/cart/{cartId}/item/{itemId}/update")
+    public ResponseEntity<ApiResponse> updateItemQuantity(@PathVariable Long cartId,
+                                                          @PathVariable Long itemId,
+                                                          @RequestParam Integer quantity) {
+        try {
+            cartItemService.updateProductQuantity(cartId,itemId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Quantity updated", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
